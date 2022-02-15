9718ddfbce1732b691e0bc7d4628dbedccad800d
Kerbusch <daankerbusch@gmail.com>
2022-02-15 15:18:14 +0100

    update 3.py v2

--- opdracht2/3.py
+++ opdracht2/3.py
@@ -6,18 +6,16 @@ from scipy import ndimage
 
 image = data.camera()
 
-newimage = filters.gaussian(image)
-
-edge1 = feature.canny(newimage, sigma=1.5)
-edge2 = feature.canny(newimage, sigma=2)
+edge1 = feature.canny(image, sigma=2.3, low_threshold=0.80, high_threshold=0.85, use_quantiles=True)
+edge2 = feature.canny(image, sigma=2)
 
 fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
 
-ax[0].imshow(newimage, cmap='gray')
+ax[0].imshow(image, cmap='gray')
 ax[0].set_title('standard', fontsize=20)
 
 ax[1].imshow(edge1, cmap='gray')
-ax[1].set_title('canny 1.5', fontsize=20)
+ax[1].set_title('canny 2.1 met threshold', fontsize=20)
 
 ax[2].imshow(edge2, cmap='gray')
 ax[2].set_title('canny 2', fontsize=20)
