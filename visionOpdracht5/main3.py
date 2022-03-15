8401e43b32bd0254249e23ab5423c2be2336c3f8
Kerbusch <daankerbusch@gmail.com>
2022-03-15 18:46:34 +0100

    almost done

--- visionOpdracht5/main3.py
+++ visionOpdracht5/main3.py
@@ -24,19 +24,15 @@ test_images = (test_images / 255) - 0.5
 train_images = np.expand_dims(train_images, axis=3)
 test_images = np.expand_dims(test_images, axis=3)
 
-print(test_images.shape)
-print(test_images[0].shape)
-
-t = tf.constant(train_images)
-print(tf.shape(t))
+print(train_images.shape)
 
 
 # Onze CNN
 
 # Stap 1: bepaal hoeveel filters je wilt, hoe groot je filter size moet zijn (let op je filter size mag niet te groot zijn vergeleken met je images), en wat je pool size is.
-num_filters = 1
-filter_size = 3
-pool_size = 2
+num_filters = 2
+filter_size = (28 * 31)
+pool_size = 100
 # zijn nu random getallen voor de filters
 
 # Stap 2: maak het model.
@@ -48,18 +44,22 @@ pool_size = 2
 
 model = Sequential([])
 
-model.add(Conv2D(num_filters, filter_size, data_format='channels_first', input_shape=(60000, 28, 31)))
-#model.add(MaxPooling2D(pool_size=pool_size))
-#model.add(Flatten())
-#model.add(Dense(2, activation='softmax'))
+#Conv2d
+model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu', input_shape=train_images.shape[1:]))
+#MaxPooling2d
+model.add(MaxPooling2D(pool_size=(3,3)))
+#Flatten
+model.add(Flatten())
+#Dense
+model.add(Dense(10, activation='softmax'))
 
 # Stap 3: het compilen van het model.
 # model.compile parameters: 'adam', loss='categorial_crossentropy', metrics=['accuracy']
-model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile('adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 
 # Stap 4: fit het model.
 #    Data om op te trainen: train_images, to_categorial(train_labels)
 #    epochs = 3
 #    validation_data = test_images, to_categorial(test_labels)
-model.fit(train_images, to_categorical(train_labels), epochs=3, validation_data= (test_images, to_categorical(test_labels)))
+model.fit(train_images, train_labels, epochs=3)#, validation_data= (test_images, to_categorical(test_labels)))
